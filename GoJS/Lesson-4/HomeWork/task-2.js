0a23515188ee0547e6f783d72812ee1be245d04e
sergeybrovarskiy <hntriot@gmail.com>
2015-06-23 01:18:54 +0300

    JS home work # - not done

--- GoJS/Lesson-4/HomeWork/task-2.js
+++ GoJS/Lesson-4/HomeWork/task-2.js
@@ -1,3 +1,16 @@
 /**
  * Created by Authority on 18.06.2015.
  */
+var a = prompt('Enter smth', '');
+function checkSpam(n) {
+    n.toLowerCase();
+    if (n.indexOf('spam') == -1) {
+        alert(n)
+    } else if (n.indexOf('sex') == -1) {
+        alert(n)
+    }
+    else {
+        alert('Spam detected!!!')
+    }
+}
+alert(checkSpam(a));
